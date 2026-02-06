b14d7a8552e5f4b6c1be8f0e8b6b25595aa6d67f
Pieris Christou <pierischristou@gmail.com>
2026-02-06 23:07:07 +0000

    build: configure swagger

--- AiimiPeopleDirectory/Program.cs
+++ AiimiPeopleDirectory/Program.cs
@@ -5,12 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
